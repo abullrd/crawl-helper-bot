c0b693cbb18f26bd93e8b355fff0f184f75f4e24
abullrd <abullrd@gmail.com>
2014-11-10 17:15:23 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,19 @@
-return {
-  explore = function()
-    if #crawl_state.desired_items() > 0 then
-      --
-    elseif crawl_state.explored() then
-      command('G>')
-    else
-      command('o')
-    end
-  end,
-  
-  start_turn = function ()
-    if (#crawl_state.enemies() == 0) then
-      explore()
-    else
-      crawl.mpr("Watch out!\n")
-      hit_closest()
-    end
+function explore()
+   if #crawl_state.desired_items() > 0 then
+    --
+  elseif crawl_state.explored() then
+    command('G>')
+  else
+    command('o')
   end
-}
+end
+
+function start_turn()
+  if (#crawl_state.enemies() == 0) then
+    explore()
+  else
+    crawl.mpr("Watch out!\n")
+    hit_closest()
+  end
+end
+  
