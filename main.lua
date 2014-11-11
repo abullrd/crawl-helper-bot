56524b621cf7f45244f2ac5390a0c82cacb165d8
abullrd <abullrd@gmail.com>
2014-11-10 17:02:37 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ return {
   start_turn = function ()
     crawl.mpr("IN user script!")
     if (#crawl_state.enemies() == 0) then
-      command('o')
+      command('G>')
     else
       crawl.mpr("Watch out")
       hit_closest()
